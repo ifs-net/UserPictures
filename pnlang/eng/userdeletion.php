944e740b395cc9a5821e0852c281ae4808577634
quan <quan@64bac6c5-d726-0410-ba7d-df0df7e89f8e>
2008-08-11 12:57:42 +0000

    Added UserDeletion support

---
+++ pnlang/eng/userdeletion.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package      UserPictures
+ * @version      $Id$
+ * @author       Florian Schießl
+ * @link         http://www.ifs-net.de
+ * @copyright    Copyright (C) 2008
+ * @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+define('_USERPICTURESUSERPICTURETITLE',	'Picture management');
+define('_USERPICTURESASSOCSBOTHSIDES',	'association(s) from and to the user');
+define('_USERPICTURESCATEGORIES',		'private categories');
+?>
